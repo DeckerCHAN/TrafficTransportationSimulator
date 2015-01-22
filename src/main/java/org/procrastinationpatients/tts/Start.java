f869149858ffe8a3cf0d31eb873b21e1ee9ffa35
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-23 01:02:36 +0800

    Established fundamental struct.

---
+++ src/main/java/org/procrastinationpatients/tts/Start.java
@@ -0,0 +1,10 @@
+package org.procrastinationpatients.tts;
+
+/**
+ * @Author Decker
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
