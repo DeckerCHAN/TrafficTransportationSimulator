e5ee3852fac195a235960b14d1afcb6787a8a6a6
jeffreychang <jeffreyz.chang@gmail.com>
2015-01-24 22:24:40 +0800

    add safety distance method

--- src/main/java/org/procrastinationpatients/tts/core/Line.java
+++ src/main/java/org/procrastinationpatients/tts/core/Line.java
@@ -5,27 +5,50 @@ import javafx.scene.canvas.GraphicsContext;
 import java.util.Collection;
 
 /**
- * @Author Decker
+ * @Author Decker & his father -- Jeffrey
  */
-public class Line implements Container,Drawable {
+public class Line implements Container, Drawable {
 
-    @Override
-    public Collection<Vehicle> getVehicles() {
-        return null;
-    }
+	private Vehicle[] line;
+	private int line_Length;
 
-    @Override
-    public void setVehicles() {
-
-    }
-
-	@Override
-	public int getSafetyDistanceByID(int index) {
-		return 0;
+	public Line(int num) {
+		line = new Vehicle[num];
+		this.line_Length = num;
 	}
 
 	@Override
-    public void draw( GraphicsContext gc) {
+	public Collection<Vehicle> getVehicles() {
+		return null;
+	}
 
-    }
+	@Override
+	public void setVehicles() {
+
+	}
+
+	//通过索引位置,得到与下一辆车之间的安全距离
+	@Override
+	public int getSafetyDistanceByID(int index) {
+		if (index < 0 || index >= this.line_Length)
+			return -1;
+		if (line[index] == null)
+			return 0;
+
+		int distance = index;
+		for (index++; index < this.line_Length; index++) {
+			if (line[index] != null) {
+				break;
+			}
+		}
+		if (index == this.line_Length) {
+			index--;
+		}
+		return index - distance;
+	}
+
+	@Override
+	public void draw(GraphicsContext gc) {
+
+	}
 }
--- src/main/java/org/procrastinationpatients/tts/core/Vehicle.java
+++ src/main/java/org/procrastinationpatients/tts/core/Vehicle.java
@@ -2,35 +2,35 @@ package org.procrastinationpatients.tts.core;
 
 /**
  * @Author Decker & his father -- Jeffrey
- *
  */
 public class Vehicle {
 
-	int Cur_Spd ;	//当前速度
-	int Cur_Loc ;	//当前位置
+	private int Cur_Spd;    //当前速度
+	private int Cur_Loc;    //当前位置
 
-	Container cont ; //当前所在的容器
+	Container cont; //当前所在的容器
 
 	//构造方法
 	public Vehicle(Container container) {
-		this.cont = container ;
+		this.cont = container;
 	}
 
-	public int move_Next_Location(){
-		return 0 ;
+	public int move_Next_Location() {
+		return 0;
 	}
 
 	//速度变化规则
-	public int Speed_From_VDR(int MAX_Speed){
-		if( this.Cur_Spd < MAX_Speed){
-			Cur_Spd++ ;
+	public int Speed_From_VDR(int MAX_Speed) {
+		if (this.Cur_Spd < MAX_Speed) {
+			Cur_Spd++;
 		}
-		int safety_distance = cont.getSafetyDistanceByID(Cur_Loc) ;
-		this.Cur_Spd = (safety_distance < this.Cur_Spd) ? (safety_distance) : (this.Cur_Spd) ;
-		return this.Cur_Spd ;
+		int safety_distance = cont.getSafetyDistanceByID(Cur_Loc);
+		this.Cur_Spd = (safety_distance < this.Cur_Spd) ? (safety_distance) : (this.Cur_Spd);
+		return this.Cur_Spd;
 	}
 
-	public int getCur_Spd(){
-		return this.Cur_Spd ;
+	public int getCur_Spd() {
+		return this.Cur_Spd;
 	}
+	public int getCur_Loc() { return this.Cur_Loc; }
 }
