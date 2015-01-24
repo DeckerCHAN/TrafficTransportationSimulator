c84cc77c7354580920b0d0963de3c42ccf9fcab4
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-24 16:03:58 +0800

    Remove annotation which not support by maven.

--- src/main/java/org/procrastinationpatients/tts/core/Drawable.java
+++ src/main/java/org/procrastinationpatients/tts/core/Drawable.java
@@ -1,11 +1,10 @@
 package org.procrastinationpatients.tts.core;
 
-import com.sun.istack.internal.NotNull;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
  * @Author Decker
  */
 public interface Drawable {
-    public void draw(@NotNull GraphicsContext gc);
+    public void draw(GraphicsContext gc);
 }
