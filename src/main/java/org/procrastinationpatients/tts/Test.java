d390e854bf27f028da40190c072482eb42fb3efb
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-26 10:43:47 +0800

    Enormous amount of changes. Refactored entity system through added interfaces.

--- src/main/java/org/procrastinationpatients/tts/Test.java
+++ src/main/java/org/procrastinationpatients/tts/Test.java
@@ -2,8 +2,10 @@ package org.procrastinationpatients.tts;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import org.apache.commons.io.FileUtils;
 import org.procrastinationpatients.tts.core.Engine;
 import org.procrastinationpatients.tts.core.VisualEntity;
+import org.procrastinationpatients.tts.source.ContainersLoader;
 
 import java.util.LinkedList;
 
@@ -13,25 +15,8 @@ import java.util.LinkedList;
 public class Test {
     public static void main(String[] args) {
         try {
-            Engine engine = Engine.getInstance();
-            LinkedList<VisualEntity> visualEntities = new LinkedList<>();
-            engine.setVisualEntities(visualEntities);
-
-            visualEntities.add(new VisualEntity() {
-                @Override
-                public void drawStaticGraphic(GraphicsContext gc) {
-                    gc.setStroke(Color.BLUE);
-                    gc.setLineWidth(5);
-                    gc.strokeLine(90, 10, 10, 40);
-                }
-
-                @Override
-                public void drawDynamicGraphic(GraphicsContext gc) {
-                    return;
-                }
-            });
-
-            engine.run();
+            ContainersLoader loader=new ContainersLoader(FileUtils.getFile("Sample(20100422 Network-same control strategy).xml"));
+            loader.getContainers();
         } catch (Exception e) {
             e.printStackTrace();
         }
