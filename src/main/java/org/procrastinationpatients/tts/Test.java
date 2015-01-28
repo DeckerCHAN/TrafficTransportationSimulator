a90ef1d8b02304040656d43fbccccfe5bca4e959
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-28 23:47:29 +0800

    Fixed a bad reference in Test.

--- src/main/java/org/procrastinationpatients/tts/Test.java
+++ src/main/java/org/procrastinationpatients/tts/Test.java
@@ -1,7 +1,7 @@
 package org.procrastinationpatients.tts;
 
 import org.procrastinationpatients.tts.core.Engine;
-import org.procrastinationpatients.tts.core.VisualEntity;
+import org.procrastinationpatients.tts.entities.Visible;
 
 import java.util.LinkedList;
 
@@ -12,7 +12,7 @@ public class Test {
     public static void main(String[] args) {
         try {
             Engine engine = Engine.getInstance();
-            LinkedList<VisualEntity> visualEntities = new LinkedList<>();
+            LinkedList<Visible> visualEntities = new LinkedList<>();
 
             engine.run();
         } catch (Exception e) {
