9af58caa149cb9f126b775c8a5508ff1ee1184d3
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-29 00:21:33 +0800

    Added vehicle to Lane

--- src/main/java/org/procrastinationpatients/tts/entities/Lane.java
+++ src/main/java/org/procrastinationpatients/tts/entities/Lane.java
@@ -1,7 +1,44 @@
 package org.procrastinationpatients.tts.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by decker on 15-1-28.
  */
 public class Lane {
+
+    private List<Lane> inputs;
+    private List<Lane> outputs;
+    private Vehicle [] vehicles;
+
+    public Lane(int length) {
+        this.inputs = new ArrayList<>();
+        this.outputs = new ArrayList<>();
+        this.setVehicles(new Vehicle[length]);
+    }
+
+    public List<Lane> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(List<Lane> inputs) {
+        this.inputs = inputs;
+    }
+
+    public List<Lane> getOutputs() {
+        return outputs;
+    }
+
+    public void setOutputs(List<Lane> outputs) {
+        this.outputs = outputs;
+    }
+
+    public Vehicle[] getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(Vehicle[] vehicles) {
+        this.vehicles = vehicles;
+    }
 }
---
+++ src/main/java/org/procrastinationpatients/tts/entities/Vehicle.java
@@ -0,0 +1,11 @@
+package org.procrastinationpatients.tts.entities;
+
+/**
+ * Created by decker on 15-1-28.
+ */
+public class Vehicle {
+    public Vehicle() {
+        super();
+    }
+
+}
