1cfa8238df50980e9dac4efe4fa6d264680f7841
DeckerCHAN <DeckerCHAN@gmail.com>
2015-01-28 16:25:17 +0800

    Update DrawUtils

--- src/main/java/org/procrastinationpatients/tts/utils/DrawUtils.java
+++ src/main/java/org/procrastinationpatients/tts/utils/DrawUtils.java
@@ -3,6 +3,7 @@ package org.procrastinationpatients.tts.utils;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 /**
  * @Author Decker
@@ -13,10 +14,16 @@ public class DrawUtils {
         gc.fillOval(point.getX() - (size / 2), point.getY() - (size / 2), size, size);
     }
 
-    public static void drawLineThrowTwoPoint(GraphicsContext gc, Point2D start, Point2D end, Color color,Integer width) {
+    public static void drawLine(GraphicsContext gc, Point2D start, Point2D end, Color color, Integer width) {
         gc.setStroke(color);
         gc.setLineWidth(width);
         gc.strokeLine(start.getX(), start.getY(), end.getX(), end.getY());
+
+    }
+
+    public static void drawLine(GraphicsContext gc, Line line, Color color, Integer width) {
+
+        drawLine(gc, new Point2D(line.getStartX(), line.getStartY()), new Point2D(line.getEndX(), line.getEndY()), color, width);
 
     }
 }
