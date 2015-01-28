1fb922f7cb4b19a7b83aed802be6814f6efc7b01
jeffreychang <jeffreyz.chang@gmail.com>
2015-01-28 17:43:44 +0800

    Changed variables in Lane

--- src/main/java/org/procrastinationpatients/tts/core/Lane.java
+++ src/main/java/org/procrastinationpatients/tts/core/Lane.java
@@ -5,30 +5,34 @@ package org.procrastinationpatients.tts.core;
  */
 public class Lane {
     Vehicle[] vehicles;
-    Lane [] input;
-    Lane [] output;
+    Lane  input;
+    Lane  output;
 	Container container ;
     public Lane()
     {
 
     }
 
-	public Lane(Container container, Lane[] input , Lane[] output)
+	public Lane(Container container, Lane input , Lane output)
 	{
 		this.container = container ;
 		this.input = input ;
 		this.output = output ;
 	}
 
-	public Lane[] getInput(){
+	public Lane getInput(){
 		return this.input ;
 	}
 
-	public Lane[] getOutput(){
+	public Lane getOutput(){
 		return this.output ;
 	}
 
 	public Container getContainer(){
 		return this.container ;
 	}
+
+	public void setInput(Lane input){ this.input = input ;}
+
+	public void setOutput(Lane output){ this.output = output ;}
 }
