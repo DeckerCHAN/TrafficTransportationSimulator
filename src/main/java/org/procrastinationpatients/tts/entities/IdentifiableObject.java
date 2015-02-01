1f78b94d5b14d37d80cabf6b0f3982451ac2bd5c
DeckerCHAN <DeckerCHAN@gmail.com>
2015-02-01 19:07:58 +0800

    Using String to store identification.

--- src/main/java/org/procrastinationpatients/tts/entities/IdentifiableObject.java
+++ src/main/java/org/procrastinationpatients/tts/entities/IdentifiableObject.java
@@ -4,12 +4,18 @@ package org.procrastinationpatients.tts.entities;
  * Created by decker on 15-1-28.
  */
 public abstract class IdentifiableObject extends Object {
-    private final Integer id;
+    private final String id;
 
-    public IdentifiableObject(Integer id) {
+    public IdentifiableObject(String id) {
         this.id = id;
     }
 
-    public Integer getId() { return id; }
+    public IdentifiableObject(Integer id) {
+        this.id = String.valueOf(id);
+    }
+
+    public String getId() {
+        return id;
+    }
 
 }
