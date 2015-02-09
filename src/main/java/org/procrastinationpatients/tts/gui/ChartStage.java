f3ece2d52d245c4b67f95bf4e1f243754f785e47
DeckerCHAN <DeckerCHAN@gmail.com>
2015-02-09 23:47:08 +0800

    Finished draw Chart.

--- src/main/java/org/procrastinationpatients/tts/gui/ChartStage.java
+++ src/main/java/org/procrastinationpatients/tts/gui/ChartStage.java
@@ -1,21 +1,65 @@
 package org.procrastinationpatients.tts.gui;
 
 
-import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import org.procrastinationpatients.tts.core.Engine;
 import org.procrastinationpatients.tts.entities.Lane;
+import org.procrastinationpatients.tts.source.StaticConfig;
+import org.procrastinationpatients.tts.utils.DrawUtils;
 
 public class ChartStage extends TickStage {
 
     private Lane[] targetLanes;
+    private Canvas[] canvases;
+    private VBox root;
+    private Long startTime;
+
 
     public ChartStage() {
         super();
+        this.startTime = System.currentTimeMillis();
         this.targetLanes = new Lane[3];
+        this.canvases = new Canvas[3];
+
+        if (StaticConfig.INSPECT_LANE_GROUP == 0) {
+            for (int i = 0; i < 3; i++) {
+                this.targetLanes[i] = Engine.getInstance().getLinks()[StaticConfig.INSPECT_LINK].getLanes()[i];
+            }
+        } else if (StaticConfig.INSPECT_LANE_GROUP == 3) {
+            for (int i = 0; i < 3; i++) {
+                this.targetLanes[i] = Engine.getInstance().getLinks()[StaticConfig.INSPECT_LINK].getLanes()[i + 3];
+            }
+        } else {
+            throw new RuntimeException("Inspect lane invalid");
+        }
+
+
+        for (int i = 0; i < this.canvases.length; i++) {
+            this.canvases[i] = new Canvas(45D, this.targetLanes[i].getLength());
+        }
+        this.root = new VBox();
+        this.root.getChildren().addAll(this.canvases);
+        this.setScene(new Scene(this.root));
+        this.getTimeline().play();
     }
 
     @Override
     protected void drawAllDynamic() {
+        for (int i = 0; i < 3; i++) {
+            Double x = (System.currentTimeMillis() - this.startTime) / 1000D;
+            for (int j = 0; j < targetLanes[i].getVehicles().length; j++) {
+                if (targetLanes[i].getVehicles()[j] != null) {
+                    Double y = (double) j;
+                    DrawUtils.drawBallAtCoordinate(canvases[i].getGraphicsContext2D(), x, y, 3, Color.BLACK);
+                }
 
+            }
+
+
+        }
     }
 
     @Override
